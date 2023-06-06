08cd55802e0f4d16ea62426c37fe932bce0a8e54
elena_is02 <elenaisaia2002@gmail.com>
2023-06-06 23:14:49 +0300

    ready in beta form

--- ElectricVehicleRoutingGui/ElectricVehicleRoutingGui.cpp
+++ ElectricVehicleRoutingGui/ElectricVehicleRoutingGui.cpp
@@ -1,10 +1,66 @@
 #include "ElectricVehicleRoutingGui.h"
 
 ElectricVehicleRoutingGui::ElectricVehicleRoutingGui(Service& service, QWidget *parent)
-    : QMainWindow(parent), service(service)
+    : QMainWindow(parent), service{ service }
 {
     ui.setupUi(this);
 }
 
 ElectricVehicleRoutingGui::~ElectricVehicleRoutingGui()
 {}
+
+//void ElectricVehicleRoutingGui::paintEvent(QPaintEvent * event)
+//{
+//    QPainter painter(this);
+//    painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::FlatCap));
+//
+//    DirectedGraphForIntersections& g = service.getInitialGraph();
+//    for (auto& list : g.getAdjacencyList()) {
+//        ChargingStation ch1 = g.getChargingStationById(list.first);
+//        const int x1 = ch1.getX();
+//        const int y1 = ch1.getY();
+//        for (auto& arch : list.second) {
+//            ChargingStation ch2 = g.getChargingStationById(arch.nextId);
+//            const int x2 = ch2.getX();
+//            const int y2 = ch2.getY();
+//            painter.drawLine(x1, y1, x2, y2);
+//        }
+//    }
+//    ///*painter.drawLine(0, 0, 200, 200);
+//
+//    //painter.drawLine(10, 0, 210, 200);
+//
+//    //painter.setPen(QPen(Qt::green, 3, Qt::SolidLine, Qt::FlatCap));
+//    //painter.drawLine(10, 0, 210, 200);*/
+//}
+
+void ElectricVehicleRoutingGui::on_changeButton_clicked()
+{
+    getInitialGraph();
+}
+
+
+void ElectricVehicleRoutingGui::on_generateButton_clicked()
+{
+
+}
+
+void ElectricVehicleRoutingGui::getInitialGraph()
+{
+    QPainter painter(this);
+    painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::FlatCap));
+
+    DirectedGraphForIntersections& g = service.getInitialGraph();
+    for (auto& list : g.getAdjacencyList()) {
+        ChargingStation ch1 = g.getChargingStationById(list.first);
+        const int x1 = ch1.getX();
+        const int y1 = ch1.getY();
+        for (auto& arch : list.second) {
+            ChargingStation ch2 = g.getChargingStationById(arch.nextId);
+            const int x2 = ch2.getX();
+            const int y2 = ch2.getY();
+            painter.drawLine(x1, y1, x2, y2);
+        }
+    }
+}
+
--- ElectricVehicleRoutingGui/ElectricVehicleRoutingGui.h
+++ ElectricVehicleRoutingGui/ElectricVehicleRoutingGui.h
@@ -6,6 +6,9 @@
 #include <QPainter>
 #include <DirectedGraphForChargingStations.h>
 #include <Service.h>
+#include <QHBoxLayout>
+#include <QTableView>
+#include <QPushButton>
 
 class ElectricVehicleRoutingGui : public QMainWindow
 {
@@ -15,33 +18,16 @@ public:
     ElectricVehicleRoutingGui(Service& service, QWidget* parent = nullptr);
     ~ElectricVehicleRoutingGui();
 
+    /*void paintEvent(QPaintEvent* event) override;*/
+
+private slots:
+    void on_changeButton_clicked();
+
+    void on_generateButton_clicked();
+
 private:
     Ui::ElectricVehicleRoutingGuiClass ui;
     Service& service;
 
-protected:
-    void paintEvent(QPaintEvent* event) override
-    {
-        QPainter painter(this);
-        painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::FlatCap));
-
-        DirectedGraphForIntersections& g = service.getInitialGraph();
-        for (auto& list : g.getAdjacencyList()) {
-            ChargingStation ch1 = g.getChargingStationById(list.first);
-            const int x1 = ch1.getX();
-            const int y1 = ch1.getY();
-            for (auto& arch : list.second) {
-                ChargingStation ch2 = g.getChargingStationById(arch.nextId);
-                const int x2 = ch2.getX();
-                const int y2 = ch2.getY();
-                painter.drawLine(x1, y1, x2, y2);
-            }
-        }
-        /*painter.drawLine(0, 0, 200, 200);
-
-        painter.drawLine(10, 0, 210, 200);
-
-        painter.setPen(QPen(Qt::green, 3, Qt::SolidLine, Qt::FlatCap));
-        painter.drawLine(10, 0, 210, 200);*/
-    }
+    void getInitialGraph();
 };
