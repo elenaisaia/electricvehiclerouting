7cb2bdbe5077e501a7e3b91a6c2cf666d21f0288
elena_is02 <elenaisaia2002@gmail.com>
2023-06-15 06:10:01 +0300

    finally a working gui; more stuff to do later

--- ElectricVehicleRoutingGui/ElectricVehicleRoutingGui.cpp
+++ ElectricVehicleRoutingGui/ElectricVehicleRoutingGui.cpp
@@ -1,9 +1,26 @@
 #include "ElectricVehicleRoutingGui.h"
 
+
+
 ElectricVehicleRoutingGui::ElectricVehicleRoutingGui(Service& service, QWidget *parent)
     : QMainWindow(parent), service{ service }
 {
     ui.setupUi(this);
+
+    mapScene = new QGraphicsScene(this);
+    mapView = findChild<QGraphicsView*>("mapView");
+
+    if (mapView) {
+        QBrush greenBrush(Qt::green);
+        QPen outlinePen(Qt::black);
+        
+        mapView->setScene(mapScene);
+    }
+
+    getInitialGraph();
+
+    qInfo() << "DRAWN POINTS:" << drawnPoints.size();
+
 }
 
 ElectricVehicleRoutingGui::~ElectricVehicleRoutingGui()
@@ -11,10 +28,10 @@ ElectricVehicleRoutingGui::~ElectricVehicleRoutingGui()
 
 //void ElectricVehicleRoutingGui::paintEvent(QPaintEvent * event)
 //{
-//    QPainter painter(this);
-//    painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::FlatCap));
+//    /*QPainter painter(this);
+//    painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::FlatCap));*/
 //
-//    DirectedGraphForIntersections& g = service.getInitialGraph();
+//    /*DirectedGraphForIntersections& g = service.getInitialGraph("smallgraph.txt");
 //    for (auto& list : g.getAdjacencyList()) {
 //        ChargingStation ch1 = g.getChargingStationById(list.first);
 //        const int x1 = ch1.getX();
@@ -25,18 +42,20 @@ ElectricVehicleRoutingGui::~ElectricVehicleRoutingGui()
 //            const int y2 = ch2.getY();
 //            painter.drawLine(x1, y1, x2, y2);
 //        }
-//    }
-//    ///*painter.drawLine(0, 0, 200, 200);
+//    }*/
+//    /*painter.drawLine(0, 0, 200, 200);
 //
-//    //painter.drawLine(10, 0, 210, 200);
+//    painter.drawLine(10, 0, 210, 200);
 //
-//    //painter.setPen(QPen(Qt::green, 3, Qt::SolidLine, Qt::FlatCap));
-//    //painter.drawLine(10, 0, 210, 200);*/
+//    painter.setPen(QPen(Qt::green, 3, Qt::SolidLine, Qt::FlatCap));
+//    painter.drawLine(10, 0, 210, 200);*/
 //}
 
 void ElectricVehicleRoutingGui::on_changeButton_clicked()
 {
-    getInitialGraph();
+    qInfo() << "Changing nodes";
+    clearSelectedNodes();
+    enableAllGraphNodeUi();
 }
 
 
@@ -45,22 +64,87 @@ void ElectricVehicleRoutingGui::on_generateButton_clicked()
 
 }
 
+
+
 void ElectricVehicleRoutingGui::getInitialGraph()
 {
-    QPainter painter(this);
-    painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::FlatCap));
+    
 
-    DirectedGraphForIntersections& g = service.getInitialGraph();
+    DirectedGraphForIntersections& g = service.getInitialGraph("generatedgraph.txt");
     for (auto& list : g.getAdjacencyList()) {
-        ChargingStation ch1 = g.getChargingStationById(list.first);
-        const int x1 = ch1.getX();
-        const int y1 = ch1.getY();
+        int x1, y1, x2, y2, id1, id2;
+        bool isCh1 = false, isCh2 = false;
+        if (g.containsChargingStation(list.first)) {
+            ChargingStation ch1 = g.getChargingStationById(list.first);
+            x1 = ch1.getX();
+            y1 = ch1.getY();
+            id1 = ch1.getId();
+            isCh1 = true;
+        }
+        else {
+            Intersection i1 = g.getIntersectionById(list.first);
+            x1 = i1.getX();
+            y1 = i1.getY();
+            id1 = i1.getId();
+            isCh1 = false;
+        }
+        
         for (auto& arch : list.second) {
-            ChargingStation ch2 = g.getChargingStationById(arch.nextId);
-            const int x2 = ch2.getX();
-            const int y2 = ch2.getY();
-            painter.drawLine(x1, y1, x2, y2);
+            if (g.containsChargingStation(arch.nextId)) {
+                ChargingStation ch2 = g.getChargingStationById(arch.nextId);
+                x2 = ch2.getX();
+                y2 = ch2.getY();
+                id2 = ch2.getId();
+                isCh2 = true;
+            }
+            else {
+                Intersection i2 = g.getIntersectionById(arch.nextId);
+                x2 = i2.getX();
+                y2 = i2.getY();
+                id2 = i2.getId();
+                isCh2 = false;
+            }
+            
+           
+           
+            if (!drawnPoints[id1]) {
+                auto point1 = new GraphNodeUi(this, id1, x1, y1, 10, 10);
+                if (isCh1) {
+                    point1->setPen(Qt::NoPen);
+                    point1->setBrush(Qt::darkGreen);
+                }
+                else {
+                    point1->setPen(Qt::NoPen);
+                    point1->setBrush(Qt::black);
+                }
+                mapScene->addItem(point1);
+                drawnPoints[id1] = point1;
+            }
+
+            if (!drawnPoints[id2]) {
+                auto point2 = new GraphNodeUi(this, id2, x2, y2, 10, 10);
+                if (isCh2) {
+                    point2->setPen(Qt::NoPen);
+                    point2->setBrush(Qt::darkGreen);
+                }
+                else {
+                    point2->setPen(Qt::NoPen);
+                    point2->setBrush(Qt::black);
+                }
+                mapScene->addItem(point2);
+                drawnPoints[id2] = point2;
+            }
+
+            mapScene->addLine(x1, y1, x2, y2);
+
+            
         }
     }
+    /*QPen outlinePen(Qt::green);
+    mapScene->addLine(-53, -217, -69, -66, outlinePen);
+    mapScene->addLine(-69, -66, -100, 7, outlinePen);
+    mapScene->addLine(-100, 7, 10, 103, outlinePen);
+    mapScene->addLine(10, 103, 90, 92, outlinePen);
+    mapScene->addLine(90, 92, 37, 214, outlinePen);*/
 }
 
--- ElectricVehicleRoutingGui/ElectricVehicleRoutingGui.h
+++ ElectricVehicleRoutingGui/ElectricVehicleRoutingGui.h
@@ -9,6 +9,10 @@
 #include <QHBoxLayout>
 #include <QTableView>
 #include <QPushButton>
+#include <qgraphicsview.h>
+#include <qgraphicsscene>
+
+#include <qgraphicsitem.h>
 
 class ElectricVehicleRoutingGui : public QMainWindow
 {
@@ -18,8 +22,9 @@ public:
     ElectricVehicleRoutingGui(Service& service, QWidget* parent = nullptr);
     ~ElectricVehicleRoutingGui();
 
-    /*void paintEvent(QPaintEvent* event) override;*/
+    //void paintEvent(QPaintEvent* event) override;
 
+    
 private slots:
     void on_changeButton_clicked();
 
@@ -29,5 +34,86 @@ private:
     Ui::ElectricVehicleRoutingGuiClass ui;
     Service& service;
 
+    QGraphicsScene* mapScene;
+    QGraphicsView* mapView;
+
     void getInitialGraph();
+
+    void nodeClicked(int nodeId) {
+        qInfo() << "Pressed graph node " << nodeId;
+
+        if (!isSourceSelected()) {
+            sourceId = nodeId;
+            return;
+        }
+
+        if (!isDestinationSelected()) {
+            destinationId = nodeId;
+            disableAllGraphNodeUi();
+
+            // aici apelez Dijkstra din service
+            qInfo() << "Source = " << sourceId << " Destination = " << destinationId;
+        }
+
+    }
+
+    class GraphNodeUi;  
+    std::unordered_map<unsigned int, GraphNodeUi*> drawnPoints;
+    int sourceId = -1;
+    int destinationId = -1;
+
+    bool isSourceSelected() {
+        return sourceId != -1;
+    }
+
+    bool isDestinationSelected() {
+        return destinationId != -1;
+    }
+
+    void clearSelectedNodes() {
+        sourceId = -1;
+        destinationId = -1;
+    }
+
+    void enableAllGraphNodeUi() {
+        for (auto mapPair : drawnPoints) {
+            mapPair.second->enableClick();
+        }
+    }
+
+    void disableAllGraphNodeUi() {
+        for (auto mapPair : drawnPoints) {
+            mapPair.second->disableClick();
+        }
+    }
+
+    class GraphNodeUi : public QGraphicsEllipseItem {
+    public:
+       
+        explicit GraphNodeUi(ElectricVehicleRoutingGui* mainWindow, int nodeId, qreal x, qreal y, qreal w, qreal h, QGraphicsItem* parent = nullptr)
+            : QGraphicsEllipseItem(x-w/2, y-h/2, w, h, parent), mainWindow{ mainWindow }, nodeId{ nodeId }  {
+
+        }
+        ~GraphNodeUi() = default;
+
+        void mousePressEvent(QGraphicsSceneMouseEvent* event) override {
+            disableClick();
+            mainWindow->nodeClicked(nodeId);
+        }
+
+        void disableClick() {
+            QGraphicsItem::setEnabled(false);
+        }
+
+        void enableClick() {
+            QGraphicsItem::setEnabled(true);
+        }
+        
+
+        private:
+
+            ElectricVehicleRoutingGui* mainWindow;
+            int nodeId = -1;
+    };
+    
 };
